d6a6fa29672a7ef606972d1b1f6efbad5033db3c
unie12 <joedaehui1@naver.com>
2025-02-17 15:42:49 +0900

    🚧 chore : 프로젝트 초기 설정
    - Spring Boot 프로젝트 구성
    - MySQL, Redis, Kafka 환경 설정
    - dependency 추가
    - issue, pr 템플릿 추가

---
+++ src/main/java/com/mindmate/mindmate_server/MindmateServerApplication.java
@@ -0,0 +1,13 @@
+package com.mindmate.mindmate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmateServerApplication.class, args);
+	}
+
+}
