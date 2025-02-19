e5221665cc1605fbbc6acdf38e1793e095102310
unie12 <joedaehui1@naver.com>
2025-02-19 17:13:36 +0900

    🌟 feat : 로그인/회원가입 부가 기능
    - 이메일 인증 서비스
    - 로그인 시도 제한 서비스
    - 로그인/회원가입 dto
    - 비밀번호 검증 및 제한
    
    Related to #4

---
+++ src/main/java/com/mindmate/mindmate_server/auth/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.mindmate.mindmate_server.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/mindmate/mindmate_server/auth/dto/SignUpRequest.java
@@ -0,0 +1,29 @@
+package com.mindmate.mindmate_server.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpRequest {
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "확인 비밀번호를 입력해주세요.")
+    private String confirmPassword;
+
+    @Builder
+    public SignUpRequest(String email, String password, String confirmPassword) {
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+}
