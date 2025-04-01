e21ad4114714d9d00361c64f6da17c60cbf3a170
unie12 <joedaehui1@naver.com>
2025-04-01 15:30:33 +0900

    🌟 feat : admin 신고 서비스/레포지토리 코드
    - 복잡한 쿼리 querydsl 이용
    
    Relate to #35

--- src/main/java/com/mindmate/mindmate_server/report/repository/ReportRepository.java
+++ src/main/java/com/mindmate/mindmate_server/report/repository/ReportRepository.java
@@ -6,7 +6,10 @@ import com.mindmate.mindmate_server.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Long> {
+public interface ReportRepository extends JpaRepository<Report, Long>, ReportRepositoryCustom {
     boolean existsByReporterAndReportedUserAndReportTargetAndTargetId(User reporter, User reportedUser, ReportTarget target, Long targetId);
+    long countByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
