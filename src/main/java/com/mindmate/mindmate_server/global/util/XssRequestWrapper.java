e1c756cf8678cfc7701f11730d47a8b576977554
unie12 <joedaehui1@naver.com>
2025-04-04 15:40:53 +0900

    🌟 feat : xss 보안 추가
    - request/header의 악성 코드 제거
    
    Relate to #46

---
+++ src/main/java/com/mindmate/mindmate_server/global/util/XssRequestWrapper.java
@@ -0,0 +1,50 @@
+package com.mindmate.mindmate_server.global.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+public class XssRequestWrapper extends HttpServletRequestWrapper {
+
+    public XssRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String[] getParameterValues(String parameter) {
+        String[] values = super.getParameterValues(parameter);
+        if (values == null) {
+            return null;
+        }
+
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = cleanXSS(values[i]);
+        }
+        return encodedValues;
+    }
+
+    @Override
+    public String getParameter(String parameter) {
+        String value = super.getParameter(parameter);
+        if (value == null) {
+            return null;
+        }
+        return cleanXSS(value);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        if (value == null) {
+            return null;
+        }
+        return cleanXSS(value);
+    }
+
+    private String cleanXSS(String value) {
+        return Jsoup.clean(value, Safelist.basic());
+    }
+}
