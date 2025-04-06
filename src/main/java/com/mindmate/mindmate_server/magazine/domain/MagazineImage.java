e1c6879c3145b04b4ee056f1a04733ec7ced0412
unie12 <joedaehui1@naver.com>
2025-04-06 09:44:14 +0900

    🌟 feat : 매거진 이미지 도메인 설정
    - 관련 dto 추가
    - 매거진 이미지 컬럼 추가
    - 관련 메서드 추가
    
    Relate to #52

--- src/main/java/com/mindmate/mindmate_server/magazine/domain/MagazineImage.java
+++ src/main/java/com/mindmate/mindmate_server/magazine/domain/MagazineImage.java
@@ -3,6 +3,7 @@ package com.mindmate.mindmate_server.magazine.domain;
 import com.mindmate.mindmate_server.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,11 +17,37 @@ public class MagazineImage extends BaseTimeEntity {
     private Long id;
 
     @Column(nullable = false)
+    private String originalName;
+
+    @Column(nullable = false)
+    private String storedName;
+
+    @Column(nullable = false)
     private String imageUrl;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private long fileSize;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "magazine_id")
     private Magazine magazine;
 
     // todo: 추가 필요 내용 + method, builder
+
+    @Builder
+    public MagazineImage(String originalName, String storedName, String imageUrl, String contentType, long fileSize, Magazine magazine) {
+        this.originalName = originalName;
+        this.storedName = storedName;
+        this.imageUrl = imageUrl;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.magazine = magazine;
+    }
+
+    public void setMagazine(Magazine magazine) {
+        this.magazine = magazine;
+    }
 }
