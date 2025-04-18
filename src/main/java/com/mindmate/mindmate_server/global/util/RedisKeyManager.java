410a1c07e63fc0b9230ac23a57ec94b232f560ab
unie12 <joedaehui1@naver.com>
2025-04-18 14:26:34 +0900

    🌟 feat : 필터링 컨슈머 추가
    - 채팅 필터링 식별 시 count 및 내용 redis에 저장
    
    Relate to #70

--- src/main/java/com/mindmate/mindmate_server/global/util/RedisKeyManager.java
+++ src/main/java/com/mindmate/mindmate_server/global/util/RedisKeyManager.java
@@ -28,4 +28,15 @@ public class RedisKeyManager {
     public String getReadStatusKey(Long roomId, Long userId) {
         return "chat:room:" + roomId + ":read:" + userId;
     }
+
+    // 사용자별 채팅방 필터링 횟수 키
+    public String getFilteringCountKey(Long userId, Long roomId) {
+        return "filtering:count:" + userId + ":" + roomId;
+    }
+
+    // 필터링 내용 저장 키? todo: 해당 내용을 따로 db에 저장을 해야할까 아니면 짧게짧게 redis로만 관리할까
+    public String getFilteringContentKey(Long userId, Long roomId) {
+        return "filtering:content:" + userId + ":" + roomId;
+    }
+
 }
