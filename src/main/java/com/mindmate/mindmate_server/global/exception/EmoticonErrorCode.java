e571f398d58da483cdab0871e580ae746fe45cf5
unie12 <joedaehui1@naver.com>
2025-04-28 14:32:17 +0900

    🌟 feat : 채팅 이모티콘 전송 기능 추가
    - 이모티콘 전송 기능
    - 이모티콘 관련 dto
    
    Relate to #80

--- src/main/java/com/mindmate/mindmate_server/global/exception/EmoticonErrorCode.java
+++ src/main/java/com/mindmate/mindmate_server/global/exception/EmoticonErrorCode.java
@@ -12,7 +12,9 @@ public enum EmoticonErrorCode implements ErrorCode {
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 파일 형식입니다."),
     FILE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "파일 크기가 너무 큽니다."),
     ALREADY_PURCHASED(HttpStatus.BAD_REQUEST, "이미 구매한 이모티콘입니다."),
-    INSUFFICIENT_POINTS(HttpStatus.BAD_REQUEST, "포인트가 부족합니다.");
+    INSUFFICIENT_POINTS(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
+    EMOTICON_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "이모티콘에 접근 권한이 없습니다" ), 
+    EMOTICON_SEND_FAILED(HttpStatus.BAD_REQUEST, "이모티콘 전송에 실패했습니다");
 
     private final HttpStatus status;
     private final String message;
