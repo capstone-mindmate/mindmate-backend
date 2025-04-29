f7e7afc698e20e09e91cf759cd6b19195281bfdc
unie12 <joedaehui1@naver.com>
2025-04-29 16:00:39 +0900

    🎉 update : 매거진 dto, 및 엔티티 수정
    - dto 생성자 추가
    - 인기 매거진 조회 시 카테고리 값 수정
    
    Relate to #94

--- src/main/java/com/mindmate/mindmate_server/magazine/dto/MagazineEngagementRequest.java
+++ src/main/java/com/mindmate/mindmate_server/magazine/dto/MagazineEngagementRequest.java
@@ -1,10 +1,14 @@
 package com.mindmate.mindmate_server.magazine.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MagazineEngagementRequest {
     private Long dwellTime; // 체류 시간
     private Double scrollPercentage; // 스크롤 얼마나 깊이 내렸는지
