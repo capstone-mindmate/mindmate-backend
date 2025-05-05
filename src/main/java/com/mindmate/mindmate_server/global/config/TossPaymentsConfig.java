5767f2804b105e8fc904b5f4ac48fcc04ebdc8c1
1-J-1 <nowijnah15@gmail.com>
2025-05-05 14:02:40 +0900

    🚧 chore : payments setting
    - toss payments config
    
    Related to #108

---
+++ src/main/java/com/mindmate/mindmate_server/global/config/TossPaymentsConfig.java
@@ -0,0 +1,25 @@
+package com.mindmate.mindmate_server.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ConfigurationProperties(prefix = "toss.payments")
+@Getter
+@Setter
+public class TossPaymentsConfig {
+    private String clientKey;
+    private String secretKey;
+    private String successCallbackUrl;
+    private String failCallbackUrl;
+    private String confirmUrl;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
