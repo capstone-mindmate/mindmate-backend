dd1623be626a3882300c565d6b6053825fe46830
1-J-1 <nowijnah15@gmail.com>
2025-05-19 22:45:33 +0900

    update : review tag 통일
    - 리뷰 태그 통일 및 관련 로직 삭제
    
    Related to #159

--- src/main/java/com/mindmate/mindmate_server/review/domain/Tag.java
+++ src/main/java/com/mindmate/mindmate_server/review/domain/Tag.java
@@ -8,20 +8,16 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum Tag {
-    // 리스너 태그
-    RESPONSIVE(TagType.LISTENER, "응답이 빨라요"),
-    EMPATHETIC(TagType.LISTENER, "공감을 잘해줘요"),
-    TRUSTWORTHY(TagType.LISTENER, "신뢰할 수 있는 대화였어요"),
-    FRESH_PERSPECTIVE(TagType.LISTENER, "새로운 관점을 제시해줘요"),
-    DETAILED_ADVICE(TagType.LISTENER, "솔직하고 현실적인 조언을 해줘요"),
-    COMFORTABLE(TagType.LISTENER, "편안한 분위기에서 이야기할 수 있었어요"),
+    RESPONSIVE("응답이 빨라요"),
+    EMPATHETIC("공감을 잘해줘요"),
+    TRUSTWORTHY("신뢰할 수 있는 대화였어요"),
+    FRESH_PERSPECTIVE("새로운 관점을 제시해줘요"),
+    DETAILED_ADVICE("솔직하고 현실적인 조언을 해줘요"),
+    COMFORTABLE("편안한 분위기에서 이야기할 수 있었어요"),
+    CLEAR_COMMUNICATION("의사소통이 명확해요"),
+    OPEN_MINDED("열린 마음으로 대화해요"),
+    RESPECTFUL("존중하는 태도로 대화해요");
 
-    // 스피커 태그
-    CLEAR_COMMUNICATION(TagType.SPEAKER, "의사소통이 명확해요"),
-    OPEN_MINDED(TagType.SPEAKER, "열린 마음으로 대화해요"),
-    RESPECTFUL(TagType.SPEAKER, "존중하는 태도로 대화해요");
-
-    private final TagType type;
     private final String content;
 
     public static Tag fromContent(String content) {
--- src/main/java/com/mindmate/mindmate_server/review/service/AdminReviewService.java
+++ src/main/java/com/mindmate/mindmate_server/review/service/AdminReviewService.java
@@ -1,11 +1,7 @@
 package com.mindmate.mindmate_server.review.service;
 
-import com.mindmate.mindmate_server.review.domain.TagType;
 import com.mindmate.mindmate_server.review.dto.ReviewResponse;
 import org.springframework.data.domain.Page;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 public interface AdminReviewService {
 
